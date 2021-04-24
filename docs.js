d24c8e969af001177b1c49267b55d3ce3fac8666
TheDrone7 <hsbhatia765@gmail.com>
2021-04-25 03:21:21 +0530

    v5 first iteration ready to publish

--- docs.js
+++ docs.js
@@ -13,8 +13,9 @@ for (const dataObj of jsonData)
     classes.push(dataObj);
 
 for (const dClass of classes) {
-  const methods = jsonData.filter(d => d.id.startsWith(dClass.id) && (d.kind === 'constructor' || d.kind === 'function'));
-  output[dClass.name] = [dClass, ...methods];
+  const methods = jsonData.filter(d => d.id.startsWith(dClass.id + '#') && (d.kind === 'function'));
+  const constructor = jsonData.filter(d => d.name === dClass.name && (d.kind === 'constructor'));
+  output[dClass.name] = [dClass, ...methods, ...constructor];
 }
 
 for (const k of Object.keys(output))
